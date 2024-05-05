ddeaf39a820b86be3bb3b84bd71d5ac06c7f4fbe
unknown <izzetomarovcff@gmail.com>
2024-05-06 01:18:59 +0400

    update

---
+++ query.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `book_library`.`book_library`  (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `book_name` varchar(255) NOT NULL,
+  `book_author` varchar(255) NOT NULL,
+  `summary` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`)
+);
